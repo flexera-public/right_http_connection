acf5714081ac6d51f929403f35d7edc23ea50f6c
Daniel Rife <daniel.rife@rightscale.com>
2013-01-16 14:35:55 -0800

    acu72727 - code review response

--- lib/right_http_connection.rb
+++ lib/right_http_connection.rb
@@ -166,7 +166,7 @@ them.
 
     def get_proxy_info_for_env
       parsed_uri = URI.parse(ENV['HTTP_PROXY'])
-      if parsed_uri.scheme == 'http'
+      if parsed_uri.scheme.to_s.downcase == 'http'
         return parsed_uri.host, parsed_uri.port, parsed_uri.user, parsed_uri.password
       else
         @logger.warn "Invalid protocol in ENV['HTTP_PROXY'] URI = #{ENV['HTTP_PROXY'].inspect} expecting 'http' got #{parsed_uri.scheme.inspect}"
