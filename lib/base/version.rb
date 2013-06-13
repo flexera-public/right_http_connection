b090f9a791e89e1bbfe2de36492642808b524f36
konstantin <konstantin@rightscale.com>
2013-06-13 13:50:58 -0700

    acu90767 - Version 1.4.0

--- lib/base/version.rb
+++ lib/base/version.rb
@@ -24,8 +24,8 @@
 module RightHttpConnection #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 1  unless defined?(MAJOR)
-    MINOR = 3  unless defined?(MINOR)
-    TINY  = 1  unless defined?(TINY)
+    MINOR = 4  unless defined?(MINOR)
+    TINY  = 0  unless defined?(TINY)
 
     STRING = [MAJOR, MINOR, TINY].join('.') unless defined?(STRING)
   end
